edd8be4bbdc50a3c28dc3640187ce92b19c72bf0
Fokiruna <alekseyyarchuk@gmail.com>
2017-11-22 20:02:29 +0300

    Add owin

---
+++ MyStoreWebApi.Api/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MyStoreWebApi.Api.Startup))]
+
+namespace MyStoreWebApi.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
