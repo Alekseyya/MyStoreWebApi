125715a630e016b9970e97ee0c40ebdbdd1a5133
Fokiruna <alekseyyarchuk@gmail.com>
2017-11-22 19:34:44 +0300

    Add service: Order, Product

---
+++ MyStoreWebApi.BL/Services/OrderService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using MyStoreWebApi.BL.Models;
+using MyStoreWebApi.BL.Services.Interfaces;
+using MyStoreWebApi.DAL.Repositories.Base;
+using MyStoreWebApi.DL.Entities;
+
+namespace MyStoreWebApi.BL.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void AddItem(OrderDTO item)
+        {
+            var order = Mapper.Map<OrderDTO, Order>(item);
+            _unitOfWork.OrderRepository.Create(order);
+        }
+
+        public void DeleteItem(OrderDTO item)
+        {
+            var user = Mapper.Map<UserDTO, User>(item.User);
+            var findOrder = _unitOfWork.OrderRepository.GetAll()
+                .FirstOrDefault(or => or.User == user && or.OrderDate == item.OrderDate);
+                
+            if (findOrder != null)
+            {
+                _unitOfWork.OrderRepository.Delete(findOrder.Id);
+            }
+        }
+
+        public void DeleteItemById(int id)
+        {
+            
+        }
+
+        public IList<OrderDTO> GetAll()
+        {
+            var order = Mapper.Map<IQueryable<Order>, IList<OrderDTO>>(_unitOfWork.OrderRepository.GetAll());
+            return order;
+        }
+
+        public OrderDTO GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(OrderDTO item)
+        {
+            var order = Mapper.Map<OrderDTO, Order>(item);
+            _unitOfWork.OrderRepository.Update(order);
+        }
+    }
+}
---
+++ MyStoreWebApi.BL/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using MyStoreWebApi.BL.Models;
+using MyStoreWebApi.BL.Services.Interfaces;
+using MyStoreWebApi.DAL.Repositories.Base;
+using MyStoreWebApi.DL.Entities;
+
+namespace MyStoreWebApi.BL.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void AddItem(ProductDTO item)
+        {
+            var product = Mapper.Map<ProductDTO, Product>(item);
+            _unitOfWork.ProductRepository.Create(product);
+        }
+
+        public void DeleteItem(ProductDTO item)
+        {
+            var product = Mapper.Map<ProductDTO, Product>(item);
+            var findProduct = _unitOfWork.ProductRepository.GetAll()
+                .FirstOrDefault(pr => pr.Name == item.Name);
+
+            if (findProduct != null)
+            {
+                _unitOfWork.ProductRepository.Delete(findProduct.Id);
+            }
+        }
+
+        public void DeleteItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductDTO> GetAll()
+        {
+            var product = Mapper.Map<IQueryable<Product>, IList<ProductDTO>>(_unitOfWork.ProductRepository.GetAll());
+            return product;
+        }
+
+        public ProductDTO GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(ProductDTO item)
+        {
+            var product = Mapper.Map<ProductDTO, Product>(item);
+            _unitOfWork.ProductRepository.Update(product);
+        }
+    }
+}
