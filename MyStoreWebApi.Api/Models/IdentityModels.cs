9b3ecf610ac485147f8ad581ceca8d5cead42674
Fokiruna <alekseyyarchuk@gmail.com>
2017-11-24 12:09:59 +0300

    Add gitignore

---
+++ MyStoreWebApi.Api/Models/IdentityModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MyWebAPI.Api.Models
+{
+    public class CustomUserRole : IdentityUserRole<Guid> { }
+    public class CustomUserClaim : IdentityUserClaim<Guid> { }
+    public class CustomUserLogin : IdentityUserLogin<Guid> { }
+
+    public class CustomRole : IdentityRole<Guid, CustomUserRole>
+    {
+        public CustomRole() { }
+        public CustomRole(string name) { Name = name; }
+    }
+
+    public class CustomUserStore : UserStore<ApplicationUser, CustomRole, Guid,
+        CustomUserLogin, CustomUserRole, CustomUserClaim>
+    {
+        public CustomUserStore(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class CustomRoleStore : RoleStore<CustomRole, Guid, CustomUserRole>
+    {
+        public CustomRoleStore(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class ApplicationUser : IdentityUser<Guid, CustomUserLogin, CustomUserRole,
+        CustomUserClaim>
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync
+            (UserManager<ApplicationUser, Guid> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this,
+                DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser, CustomRole,
+        Guid, CustomUserLogin, CustomUserRole, CustomUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("MyStore")
+        {
+
+        }
+    }
+}
